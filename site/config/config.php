d05a601013060d6a5262cca9a775cab3f8e1c665
Andi <hallo@andimeier.ch>
2026-05-14 17:57:56 +0200

    feat(favicons): add panel favicons

--- site/config/config.php
+++ site/config/config.php
@@ -7,4 +7,29 @@ return [
             'active' => false,
         ],
    ],
+    'panel' => [
+        'favicon' => [
+            [
+                'rel'  => 'icon',
+                'type' => 'image/png',
+                'href' => 'favicon-96x96.png',
+                'sizes' => '96x96',
+            ],
+            [
+                'rel'  => 'icon',
+                'type' => 'image/svg+xml',
+                'href' => 'favicon.svg',
+            ],
+            [
+                'rel'  => 'shortcut icon',
+                'href' => 'favicon.ico',
+            ],
+            [
+                'rel'  => 'apple-touch-icon',
+                'type' => 'image/png',
+                'href' => 'apple-touch-icon.png',
+                'sizes' => '180x180',
+            ],
+        ],
+    ],
 ];
